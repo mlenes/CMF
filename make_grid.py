a090861af78449cf40b29f8f0b4b77987298181c
Linda Dierikx <l.dierikx@student.tudelft.nl>
2025-02-21 14:33:45 +0100

    grid with layers

--- make_grid.py
+++ make_grid.py
@@ -1,1 +1,14 @@
 # make 1D grid
+import numpy as np
+
+def vertical_dist(N,dy,Nlayers,layerfactor):
+    vertical_distance=np.ones(N-1)*dy
+    if Nlayers>0:
+        for i in range(Nlayers-1):
+            vertical_distance[i]=vertical_distance[i]/(layerfactor**(Nlayers-i))
+            vertical_distance[-(i+1)]=vertical_distance[-(i+1)]/(layerfactor**(Nlayers-i))
+    return vertical_distance
+
+
+            
+    
