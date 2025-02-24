38690541f8639b1a97177eb3fb463ee34c0a01eb
ldierikx <lindadierikx@gmail.com>
2025-02-24 15:42:36 +0100

    add neumann boundary conditions to assembly

--- assembly.py
+++ assembly.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def assemble_matrix(N, L, mu, p_grad, bndry_type, u_wall):
+def assemble_matrix(N, L, mu, p_grad, bndry_type, u_wall, grad_wall):
 	dy = L/(N-1)
 	A = np.zeros((N-2, N-2)) # Coefficient matrix
 	b = np.full(N-2, p_grad) # Right hand side
@@ -24,5 +24,17 @@ def assemble_matrix(N, L, mu, p_grad, bndry_type, u_wall):
 
 		b[0] = u_wall
 		b[-1] = u_wall
+
+	if bndry_type == 'neumann':
+		A[0] = np.zeros(A[0].shape)
+		A[0,0] = 1
+		A[0,1] = -1
+
+		A[-1] = np.zeros(A[0].shape)
+		A[-1,-1] = 1
+		A[-1,-1] = -1
+
+		b[0] = - grad_wall*dy
+		b[-1] = grad_wall*dy
 
 	return A, b
