93bfb53df5b975db6e274c307b9baadec9920060
ldierikx <lindadierikx@gmail.com>
2025-02-24 15:44:05 +0100

    Update main.py for neumann boundary conditions

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
 	# Include the options from the parser
 	opts = get_options()
 	mu = make_grid.init_mu(opts.N, opts.mu)
-	A, b = assembly.assemble_matrix(opts.N, opts.L, mu, opts.p_grad, opts.bndry_type, opts.wallspeed)
+	A, b = assembly.assemble_matrix(opts.N, opts.L, mu, opts.p_grad, opts.bndry_type, opts.wallspeed, opts.wallgradient)
 	u = np.linalg.solve(A, b)
 	visuals.visualize(u)
 
