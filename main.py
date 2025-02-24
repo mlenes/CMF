f682260dfed5024a09f37f018f58e8a24e26ce67
mlenes <m.m.lenes@student.tudelft.nl>
2025-02-24 11:32:41 +0100

    Small change to main

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
 	# Include the options from the parser
 	opts = get_options()
 	mu = make_grid.init_mu(opts.N, opts.mu)
-	A, b = assembly.assemble_matrix(opts.N, opts.L, mu, opts.p_grad, opts.bndry_type, 0)
+	A, b = assembly.assemble_matrix(opts.N, opts.L, mu, opts.p_grad, opts.bndry_type, opts.wallspeed)
 	u = np.linalg.solve(A, b)
 	visuals.visualize(u)
 
