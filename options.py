42dba36200486a1ffd1ffafe5969755db673cffe
Linda Dierikx <l.dierikx@student.tudelft.nl>
2025-02-28 13:47:34 +0100

    added global boundary option

--- options.py
+++ options.py
@@ -3,19 +3,20 @@ import argparse
 def get_options():
 	"""
 	Use this in the terminal like python3 main.py --density 3.0
-	"""
-	parser = argparse.ArgumentParser(prog='Simulation of multiphase flow')
-	parser.add_argument("--density", help='Density of the fluid', type=float)
-	parser.add_argument("--iterations", help='Number of iterations', type=int, default = 100)
-	parser.add_argument("--p_grad", help='prescribed gradient in x direction', type=float, default=-0.1)
-	parser.add_argument("--Nlayers", help='How many layers for grid', type=int, default=0)
-	parser.add_argument("--layerfactor", help='How refined the grid is at the boundary', type=float, default=2)
-	parser.add_argument("--dy", help='standard grid size', type=float, default=0.01)
-	parser.add_argument("--N", help='Amount of grid points', type=int, default=50)
-	parser.add_argument("--mu", help='Molecular viscosity', type=float, default=0.001)
-	parser.add_argument("--wallspeed", help='speed of wall', type=int, default=0)
-	parser.add_argument("--bndry_type", help='Type of boundary conditions at the wall', type=str, default='dirichlet')
-	parser.add_argument("--wallgradient", help='gradient of velocity at the wall', type=float, default=-4)
+    """
+    parser = argparse.ArgumentParser(prog='Simulation of multiphase flow')
+    parser.add_argument("--density", help='Density of the fluid', type=float)
+    parser.add_argument("--iterations", help='Number of iterations', type=int, default = 100)
+    parser.add_argument("--p_grad", help='prescribed gradient in x direction', type=float, default=-0.1)
+    parser.add_argument("--Nlayers", help='How many layers for grid', type=int, default=0)
+    parser.add_argument("--layerfactor", help='How refined the grid is at the boundary', type=float, default=2)
+    parser.add_argument("--dy", help='standard grid size', type=float, default=0.01)
+    parser.add_argument("--N", help='Amount of grid points', type=int, default=50)
+    parser.add_argument("--mu", help='Molecular viscosity', type=float, default=0.001)
+    parser.add_argument("--wallspeed", help='speed of wall', type=int, default=0)
+    parser.add_argument("--bndry_type", help='Type of boundary conditions at the wall', type=str, default='dirichlet')
+    parser.add_argument("--global_type", help='Type of global boundary condition', type=str, default='pressure')
+    parser.add_argument("--wallgradient", help='gradient of velocity at the wall', type=float, default=-4)
 	parser.add_argument("--flowrate", help='Flow rate global boundary condition', type=float, default=2)
 	parser.add_argument("--L", help="Width of the channel", type=float, default=1)
 	opts = parser.parse_args()
