b6874bf60499a2a9ccc89558ce8699f23a70cc26
Linda Dierikx <l.dierikx@student.tudelft.nl>
2025-03-01 16:24:10 +0100

    fix loop for flowrate and make y in plot non-dimensional

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main():
     global_vector = np.array([1,0]) #default is given pressure gradient
     if opts.global_type=="flowrate":
         global_vector = np.array([0,1])
-    p_grad =  np.inner(global_vector, [opts.p_grad , 4*opts.flowrate/opts.L])
+    p_grad =  np.inner(global_vector, [opts.p_grad , -4*opts.flowrate/opts.L*opts.mu])
 
     if opts.flow_type == 'laminar':
         A = assembly.assemble_A(opts.N, opts.L, opts.mu, opts.bndry_bot, opts.bndry_top, opts.bndry_val_bot, opts.bndry_val_top)
@@ -18,11 +18,16 @@ def main():
         u = np.linalg.solve(A, b)
         
         if opts.global_type=="flowrate":
-            #flowrate and pressure are linearly related, so find corrected pressure gradient using calculated flowrate
-            correction_flowrate =  (np.sum(u)*opts.L) / opts.flowrate
-            corrected_p_grad = p_grad / correction_flowrate
-            b_new = assembly.assemble_b(opts.N, opts.L, opts.mu, corrected_p_grad, opts.bndry_bot, opts.bndry_top, opts.bndry_val_bot, opts.bndry_val_top)
-            u = np.linalg.solve(A, b_new)
+            error_flowrate = 10
+            while np.abs(error_flowrate) > 1e-3*opts.flowrate:
+                #find corrected pressure gradient using calculated flowrate and assuming linear relation
+                correction_flowrate =  (np.sum(u)*opts.L) / opts.flowrate
+                p_grad /= correction_flowrate
+                b_new = assembly.assemble_b(opts.N, opts.L, opts.mu, p_grad, opts.bndry_bot, opts.bndry_top, opts.bndry_val_bot, opts.bndry_val_top)
+                u = np.linalg.solve(A, b_new)
+                error_flowrate = np.sum(u)*opts.L - opts.flowrate 
+            
+            
 
     elif opts.flow_type == 'turbulent':
         for _ in range(opts.iterations):
--- visuals.py
+++ visuals.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def visualize(u):
-    plt.plot(u[1:-1], range(len(u)-2))
+    plt.plot(u[1:-1], np.linspace(0,1,len(u)-2))
     plt.xlabel("u [m/s]")
-    plt.ylabel("y[m]")
+    plt.ylabel("y/H [-]")
     plt.show()
