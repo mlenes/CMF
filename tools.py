6383972e25fe29c102668ed6c101b61ca5dac74e
mlenes <mats.lenes@gmail.com>
2025-04-29 10:30:20 +0200

    Added a sine pressure wave

--- tools.py
+++ tools.py
@@ -16,4 +16,8 @@ def calc_mixing_length(N,L):
     mixing_length = L/2 * (np.full(N,0.14) - np.full(N,0.08) * (np.ones(N)-2*rel_height)**2 - np.full(N,0.06) * (np.ones(N)-2*rel_height)**4)
     return mixing_length/L
     
+def get_sin_p(p0, T, t):
+	# Pressure difference over time with a sine
+	return A*np.sin(2*np.pi*t/T)
+    
 
