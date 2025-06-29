fixed non-dimensionalization, fixed for L different than 1, make plots of wall profile in plus units, make plot of mixing length, fix u_tau in van driest damping

iterators.py
@@ -3,30 +3,33 @@ import assembly
 import tools
 
 	
-def iter_u(iterations, u, N, L, Ks, mu_faces, rel_factor, p_grad):
+def iter_u(iterations, u, N, L, Ks, mu_faces, rel_factor, p_grad, wall_constant=None):
     for i in range(iterations):
         #each iteration first loop until velocity profile matches eddy viscosity
         u_corr = 10*np.mean(u)
-        while np.abs(np.mean(u_corr)) > np.abs(1e-5*np.mean(u)):
+
+        while np.abs(np.mean(u_corr)) > np.abs(1e-4*np.mean(u)):
+            tau_wall = mu_faces[0]*(u[1]-u[0])/(tools.get_dy(N, L)/L)
+            u_tau = np.sqrt(tau_wall)
             
-            # Get wall constant
-            u_tau = u[1] * 0.41 / (np.log((tools.get_dy(N, L)*32.6/2)/Ks))
-            tau_w = u_tau**2 #non-dimensional, so rho=1
-            wall_constant = tau_w/u[1]
+            # If you use a wall function, use log function for tau_wall
+            if wall_constant!=None:
+                u_tau = u[1] * 0.41 / (np.log((tools.get_dy(N, L)*32.6/2)/Ks))            
+                tau_w = u_tau**2 #non-dimensional, so rho=1
+                wall_constant = tau_w/u[1]
             
             # Eddy viscosity
             l_m = tools.calc_mixing_length(N, L, u_tau, mu_faces[1])
-            uprime = l_m * np.abs(u[:-1] - u[1:])/tools.get_dy(N, L)
-            mu_eff = mu_faces + uprime*l_m
+            u_prime = l_m * np.abs(u[:-1] - u[1:])/(tools.get_dy(N, L)/L)
+            mu_eff = mu_faces + u_prime*l_m
             
             # Get turbulent A and b
             A = assembly.assemble_A(N, L, mu_eff, wall_constant)
             b = assembly.assemble_b(N, L, mu_eff, p_grad)
             u_new = np.linalg.solve(A, b)
             u_corr = u_new - u
-            u += rel_factor*u_corr
-            
-    return u, uprime
+            u += rel_factor*u_corr      
+    return u, u_prime, u_tau
 
 def particle(y0, v0, u, dt, tracktime, mu, D, M, g, N, L, u_prime):
 	x_list = []
@@ -57,12 +60,13 @@ def particle(y0, v0, u, dt, tracktime, mu, D, M, g, N, L, u_prime):
 			
 			#if eddy died or particle passed through eddy, make a new one
 			if dt*i>eddytime[n]:
-				vprime[n] = tools.choose_vprime(u_prime[gridcell])
-				dUdy = np.abs(u[gridcell] - u[gridcell+1])/tools.get_dy(N, L)
-				T_e = 0.15/dUdy #eddy lifetime
-				T_r = (vprime[n]/dUdy) / np.max([np.abs(vy[i-1]),np.abs(u[gridcell]-vx[i-1])]) #residence time
-				eddytime[n]+= np.min([T_e,T_r]) #check in how much time this eddy will last
+				dUdy = np.abs(u[gridcell] - u[gridcell+1])/(tools.get_dy(N, L)/L)
+				vprime[n] = tools.choose_vprime(np.sqrt(u_prime[gridcell]*dUdy))
 				
+				T_e = 0.15/dUdy #eddy lifetime
+				T_r = (np.abs(vprime[n])/dUdy) / np.max([np.abs(vy[i-1]),np.abs(u[gridcell]-vx[i-1])]) #residence time
+				eddytime[n]+= np.min([T_e,T_r]) #check in how much time this eddy will last				
+# 				print(vprime[n],eddytime[n])
 
 			Fx = f*3*np.pi*mu*D*(u[gridcell]+vprime[n]-vx[i-1]) #now only drag force, all input is non-dimensional
 			

main.py
@@ -4,6 +4,7 @@ import assembly
 import visuals
 import tools
 import iterators
+import matplotlib.pyplot as plt
 
 def main():
 	# Include the options from the parser
@@ -13,14 +14,14 @@ def main():
 	for t in range(opts.sim_time):
 
 		p_grad = tools.get_sin_p(opts.p_grad, opts.pressure_period, t)
-		u_ref = tools.get_u_ref(p_grad, opts.L, opts.rho_ref)
+		u_ref = 6
 		
 		# Compute viscosity at the faces of the cells and make dimensionless
 		mu_faces = tools.init_mu_faces(opts.N, opts.mu)
 
 		# Make p_grad and viscosity dimensionless
-		p_grad /= 2*opts.rho_ref*u_ref
-		mu_faces /= u_ref*opts.L*opts.rho_ref
+		p_grad /= ((opts.rho_ref*u_ref**2)/opts.L)
+		mu_faces /= (u_ref*opts.L*opts.rho_ref)
 
 		# Assemble the A matrix for calculating
 		A = assembly.assemble_A(opts.N, opts.L, mu_faces)
@@ -32,23 +33,44 @@ def main():
 		u = np.linalg.solve(A, b)
 		
 		if opts.flow_type == 'turbulent':
-			u, u_prime = iterators.iter_u(opts.iterations, u, opts.N, opts.L, opts.Ks, mu_faces, opts.rel_factor, p_grad)
+			u, u_prime, u_tau = iterators.iter_u(opts.iterations, u, opts.N, opts.L, opts.Ks, mu_faces, opts.rel_factor, p_grad,1)
         
 			print("mean flow found, now adding particles for time", t)
         
+		y_plus = tools.get_dy(opts.N,opts.L)*u_tau*u_ref*opts.rho_ref/opts.mu/opts.L
+# 		print("y_plus=",y_plus)
+		
 		#gehardcode oeps
-		y0 = np.array([0.01, 0.3, 0.5]) #non-dimensional height where we insert particle in channel
-		v0 = np.array([4, 0])  #non-dimensional starting velocity
+		y0 = np.array([0.4]) #non-dimensional height where we insert particle in channel
+		v0 = np.array([1, 0])  #non-dimensional starting velocity
 		
 		M = opts.M / (opts.rho_ref*opts.L**3)  #non-dimensional weight of particle
 		g = 9.81 * opts.L/ u_ref**2  #non-dimensional gravitational acceleration
 		x_list, y_list = iterators.particle(y0,v0,u,opts.dt,opts.tracktime,mu_faces[1],opts.D,M,g,opts.N,opts.L,u_prime)
-		
 		visuals.particles(x_list*opts.L,y_list*opts.L)
-		print(u[1000])
 		u_list.append(u*u_ref)
+		
+		
+	#make a log plot of the velocity in plus units
+# 	uarray = np.array(u_list[0])
+# 	uplus = (uarray/u_ref)/u_tau
+# 	conversion = u_tau*u_ref*opts.rho_ref/opts.mu
+# 	n=50 #how many points you want to plot
+# 	visuals.wallprofile(opts.L,opts.N,n,conversion,uplus)
+    
+	#make a plot of the mixing length function
+# 	plt.figure()
+# 	channel=np.linspace(0,1,100)
+# 	mixing_length = tools.calc_mixing_length(100, 1, 100, 0.01)
+# 	plt.plot(channel,mixing_length)
+# 	plt.xlabel("y/H")
+# 	plt.ylabel("$l_m$/H")
+# 	plt.show()
+		
 	u_list = np.array(u_list)
 	visuals.visualize(u_list)
+	
+	
 
 if __name__ == "__main__":
 	main()

tools.py
@@ -3,9 +3,6 @@ import numpy as np
 def get_dy(N, L):
 	return L/N
 
-def get_u_ref(p_grad, L, rho_ref):
-	return np.sqrt(-p_grad*L/(2*rho_ref)) 
-
 def init_mu_faces(N,mu):
 	#create array of viscosity at gridpoints
 	viscosity=np.ones(N+1)*mu
@@ -14,16 +11,15 @@ def init_mu_faces(N,mu):
 
 def calc_mixing_length(N,L,u_tau,mu):
     #calculate mixing length at faces (non-dimensional)
-    first_half = np.linspace(0,L/2,int(N/2)) / L
+    first_half = np.linspace(0,L/2,int(N/2)) / (L/2)
     rel_height = np.pad(first_half,(0,int(N/2)),'symmetric')
-    mixing_length = L/2 * (np.full(N,0.14) - np.full(N,0.08) * (np.ones(N)-2*rel_height)**2 - np.full(N,0.06) * (np.ones(N)-2*rel_height)**4)
-    
+    mixing_length = 1/2 * (np.full(N,0.14) - np.full(N,0.08) * (np.ones(N)-rel_height)**2 - np.full(N,0.06) * (np.ones(N)-rel_height)**4)
     #van Driest damping
     y_plus = (rel_height * u_tau) / mu
     A_plus = 25
     D = (1-np.exp(-y_plus/A_plus))
-    
-    return mixing_length*D/L
+    # D=1
+    return mixing_length*D
     
 def get_sin_p(p0, T, t):
 	# Pressure difference over time with a sine